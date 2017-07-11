4b6609b41cab4e3eb7ccfa1592cb9c76da47feaf
Nathaniel Vacanti <Nate@NathanielVacanti.scilifelab.se>
2017-07-11 15:56:41 +0200

    added csv to tab delimited script

---
+++ csv_to_tab.py
@@ -0,0 +1,10 @@
+# Script to convert comma separated file to tab delimited file
+
+csv_file = 'group_color_key.csv'
+txt_file = csv_file[:-3] + 'txt'
+
+with open(csv_file,'r') as file_read:
+    with open(txt_file,'w') as file_write:
+        for line in file_read:
+            newline = line.replace(',','\t')
+            file_write.write(newline)
