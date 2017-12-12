4960f81a97fcf2cc9fec8035f0b2f6d614a8f509
Nathaniel Vacanti <VacantiLab@users.noreply.github.com>
2017-12-12 15:03:42 +0100

    changed the filenames

--- csv_to_tab.py
+++ csv_to_tab.py
@@ -1,6 +1,6 @@
 # Script to convert comma separated file to tab delimited file
 
-csv_file = 'rna_seq_supplemental_tab_1.csv'
+csv_file = '/Users/Nate/Dropbox/Research/Lehtio_Laboratory/Projects/breast_cancer/patient_tumors/lehtio_lab_landscape_study/Metabolomics/Tumors_with_Proteome_Quantification/group_color_key.csv'
 txt_file = csv_file[:-3] + 'txt'
 
 with open(csv_file,'r') as file_read:
